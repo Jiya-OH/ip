aa37af73bb74a7add8178ce2c0dc0c04fc8d29e0
Jiya-OH <ohjingyang@gmail.com>
2026-01-18 11:44:49 +0800

    Update ROberto.java to echo

--- src/main/java/Roberto.java
+++ src/main/java/Roberto.java
@@ -1,16 +1,35 @@
+import java.util.*;
+
 public class Roberto {
     public static void greet(){
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Roberto\n" +
-                " What can I do for you?");
+                " What can I do for you?\n" +
+                "____________________________________________________________");
     }
+
+    public static void echo(String input) {
+        System.out.println("____________________________________________________________\n" + input);
+        System.out.println("____________________________________________________________");
+    }
+
     public static void exit(){
         System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
     }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         greet();
+        while (true){
+            String input = scanner.nextLine();
+            if (input.equals("bye")){
+                break;
+            } else {
+                echo(input);
+            }
+        }
         exit();
     }
 }
