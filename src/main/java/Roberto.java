d71b04ebc81192b5b2357a3e7f909b077b635594
Jiya-OH <ohjingyang@gmail.com>
2026-01-18 12:05:38 +0800

    Update Roberto.java to store texts and display them

--- src/main/java/Roberto.java
+++ src/main/java/Roberto.java
@@ -1,35 +1,63 @@
 import java.util.*;
 
 public class Roberto {
-    public static void greet(){
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Roberto\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________");
-    }
-
-    public static void echo(String input) {
-        System.out.println("____________________________________________________________\n" + input);
+    public static List<String> list;
+    public static void printLine(){
         System.out.println("____________________________________________________________");
     }
 
+    public static void greet(){
+        printLine();
+        System.out.println(" Hello! I'm Roberto\n" +
+                           " What can I do for you?");
+        printLine();
+    }
+
+    //Adds input line to list
+    public static void addToList(String input) {
+        list.add(input);
+        printLine();
+        System.out.println(" added: " + input);
+        printLine();
+    }
+
+    //Prints the whole list
+    public static void printList(){
+        //initialize number for ordering
+        int num = 1;
+        printLine();
+        for (String line : list){
+            System.out.println(" " + num++ + ". " + line);
+        }
+        printLine();
+    }
+
     public static void exit(){
-        System.out.println("____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        printLine();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //initializes new list
+        list = new ArrayList<>();
+
         greet();
+
+        //Continuously receives input until user inputs "bye"
         while (true){
             String input = scanner.nextLine();
             if (input.equals("bye")){
                 break;
+            } else if (input.equals("list")) {
+                printList();
             } else {
-                echo(input);
+                addToList(input);
             }
         }
+
         exit();
     }
 }
